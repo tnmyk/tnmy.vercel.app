54007fdc048adc03fbb1dadc3baa3397ed573acf
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-18 02:44:52 +0530

    Added profile component

--- components/Home/Profile/Profile.tsx
+++ components/Home/Profile/Profile.tsx
@@ -1,9 +1,28 @@
+import { AiFillGithub } from "react-icons/ai";
+import { BsTwitter } from "react-icons/bs";
+import { GrLinkedinOption } from "react-icons/gr";
+import { SiDiscord } from "react-icons/si";
 const Profile = () => {
   return (
-    <div className="flex justify-between">
-      <img src="./pfp1.jpg" className="rounded-full w-32" />
+    <div className="flex justify-between items-center gap-x-16 mt-12">
+      <img src="./pfp1.jpg" className="rounded-full w-36" />
       <div>
-          <h1 className="font-medium text-2xl">tanmay kachroo</h1>
+        <h1 className="font-medium text-3xl">tanmay kachroo</h1>
+        <div className=" text-gray-200 mt-1 mb-4">fullstack dev</div>
+        <div className="flex items-center text-xl gap-3">
+          <a href="/">
+            <AiFillGithub />
+          </a>
+          <a href="/">
+            <BsTwitter />
+          </a>
+          <a href="/">
+            <GrLinkedinOption />
+          </a>
+          <a href="/">
+            <SiDiscord />
+          </a>
+        </div>
       </div>
     </div>
   );
--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -2,8 +2,8 @@ import { BsMoon } from "react-icons/bs";
 const Nav = () => {
   return (
     <nav className="flex justify-between p-10 px-14 w-1/2">
-      <div className="font-medium text-md">tnmyk</div>
-      <button className="h-8 w-8 flex items-center justify-center rounded bg-neutral-800">
+      <div className="font-medium text-lg">tnmyk</div>
+      <button className="h-10 w-10 flex items-center justify-center rounded-md bg-neutral-800">
         <BsMoon />
       </button>
     </nav>
