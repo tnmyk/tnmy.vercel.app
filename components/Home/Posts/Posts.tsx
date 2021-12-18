58cf2c714c51499809ae669433ae434ab2eb479b
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-18 21:46:38 +0530

    Link added to post cards

--- components/Home/Posts/Posts.tsx
+++ components/Home/Posts/Posts.tsx
@@ -1,13 +1,10 @@
+import Link from "next/link";
+
 const Posts = ({ posts }: { posts: Array<{ [key: string]: any }> }) => {
   return (
     <div className="flex gap-x-4">
       {posts.map((post, index) => {
-        return (
-          <PostCard
-            key={index}
-            title={post.properties.Name.title[0].plain_text}
-          />
-        );
+        return <PostCard key={index} post={post} />;
       })}
     </div>
   );
@@ -15,13 +12,15 @@ const Posts = ({ posts }: { posts: Array<{ [key: string]: any }> }) => {
 
 export default Posts;
 
-const PostCard = ({ title }: { title: string }) => {
+const PostCard = ({ post }: { post: { [key: string]: any } }) => {
   return (
-    <div
-      style={{ width: "11.3rem", height: "11.3rem" }}
-      className=" rounded-md flex items-center justify-center p-6 bg-pink-300 text-lg leading-5"
-    >
-      {title}
-    </div>
+    <Link href={`/posts/${post.id}`}>
+      <div
+        style={{ width: "11.3rem", height: "11.3rem" }}
+        className=" rounded-md flex items-center justify-center p-6 bg-pink-300 text-lg leading-5"
+      >
+        {post.properties.Name.title[0].plain_text}
+      </div>
+    </Link>
   );
 };
