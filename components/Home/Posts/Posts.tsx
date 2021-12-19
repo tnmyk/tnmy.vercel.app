619adff9943419694a4d48b7936b175d2d5d5357
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-20 03:35:06 +0530

    Postcard color, bgColor not synced with notion

--- components/Home/Posts/Posts.tsx
+++ components/Home/Posts/Posts.tsx
@@ -13,10 +13,16 @@ const Posts = ({ posts }: { posts: Array<{ [key: string]: any }> }) => {
 export default Posts;
 
 const PostCard = ({ post }: { post: { [key: string]: any } }) => {
+  console.log(post.properties);
   return (
     <Link href={`/posts/${post.id}`}>
       <div
-        style={{ width: "11.3rem", height: "11.3rem" }}
+        style={{
+          width: "11.3rem",
+          height: "11.3rem",
+          backgroundColor: post.properties.BgColor?.rich_text[0]?.plain_text,
+          color: post.properties.Color?.rich_text[0]?.plain_text,
+        }}
         className="cursor-pointer rounded-md flex items-center justify-center p-6 bg-pink-300 text-lg leading-5"
       >
         {post.properties.Name.title[0].plain_text}
