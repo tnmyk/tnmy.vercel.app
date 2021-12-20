9d91e86ea3cf4a6bf8d36e3f453abd3645e3ecf1
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-21 01:27:42 +0530

    Project responsive

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -17,7 +17,7 @@ export default Projects;
 
 const ProjectCard = ({ project }: { [key: string]: any }) => {
   return (
-    <div className="flex-col w-11/12 sm:5/12 sm:flex-row border border-neutral-700 rounded-md w-5/12 flex justify-between items-center p-2 px-4 mb-2">
+    <div className="flex-col w-11/12 sm:w-5/12 sm:flex-row border border-neutral-700 rounded-md  flex justify-between items-center p-2 px-4 mb-2">
       <div className="flex flex-col ">
         <span className="text-xl leading-5 mt-3">
           {project.properties.Name.title[0].plain_text}
