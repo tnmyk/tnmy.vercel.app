7e6dff1cf1bff0ce9f007bfdaa9f59d408649448
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-20 19:41:28 +0530

    Added cors

--- pages/api/views/[id].ts
+++ pages/api/views/[id].ts
@@ -1,14 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Client } from "@notionhq/client";
+import Cors from "cors";
 
 type Data = {
   views: number;
 };
 
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await runMiddleware(req, res, cors);
   if (req.method === "POST") {
     const { id: pageId }: any = req.query;
     const notion = new Client({ auth: process.env.NOTION_KEY });
