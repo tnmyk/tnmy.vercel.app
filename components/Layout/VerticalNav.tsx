d80be7d265ed6d38bb587e014acd5cbb0d1075cb
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-21 22:55:33 +0530

    Vertical nav Active link styling

--- components/Layout/VerticalNav.tsx
+++ components/Layout/VerticalNav.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Url } from "url";
 
 const StyledLink = ({ children, href }: { children: string; href: any }) => {
   const router = useRouter();
@@ -8,17 +7,16 @@ const StyledLink = ({ children, href }: { children: string; href: any }) => {
   return (
     <Link href={href}>
       <a
-        style={{
-          borderBottom:
-            (children === "home" &&
-              (router.asPath === "/" || router.asPath === "")) ||
-            router.asPath.includes(children)
-              ? "3px solid pink"
-              : "",
-        }}
-        className=" text-2xl w-fit"
+        className={`${
+          (children === "home" &&
+            (router.asPath === "/" || router.asPath === "")) ||
+          router.asPath.includes(children)
+            ? "verticalNavActive"
+            : ""
+        } verticalNavLink text-2xl w-fit`}
       >
         {children}
+        <span />
       </a>
     </Link>
   );
