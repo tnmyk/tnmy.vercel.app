96262ed8594a5bde86c6d0348818040616072d82
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-22 15:36:53 +0530

    Profile animation changes

--- components/Home/Profile/Profile.tsx
+++ components/Home/Profile/Profile.tsx
@@ -18,11 +18,18 @@ const Profile = () => {
     };
   }, []);
   return (
-    <div style={{ minHeight: "14rem" }} className="relative w-fit  flex justify-center">
+    <div
+      style={{ minHeight: "14rem" }}
+      className="relative w-fit  flex justify-center"
+    >
       <div id="profile" className="w-fit">
         <div className="scale-80 flex-col sm:flex-row  gap-y-6 gap-x-8 sm:scale-75 md:scale-90 lg:scale-100 flex justify-between items-center sm:gap-x-16 mt-12">
           <img src="./pfp1.jpg" className="rounded-full w-36" />
-          <div className="flex flex-col items-center sm:block">
+
+          <div
+            id="profileText"
+            className="profileText flex flex-col items-center sm:block"
+          >
             <h1 className="font-medium text-3xl w-max">tanmay kachroo</h1>
             <div className=" text-gray-200 mt-1 mb-4">fullstack dev</div>
             <div className="flex items-center text-xl gap-3">
